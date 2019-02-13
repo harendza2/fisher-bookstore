79705efe9a68808d95085d585904e5e14a1ab8c6
Max Harendza <maxharendza@gmail.com>
2019-02-13 18:23:20 -0500

    change configureServices

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -25,7 +25,8 @@ namespace Fisher.Bookstore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<BookstoreContext>(AppDomainManagerInitializationOptions => options.UseInMemoryDatabase("Books"));
+            services.AddMvc().SetCompatibilityVersion(CompatabilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
